754c80940ba87a25da280be5fd8d02d14c9d18d1
MA_THINKPAD <yoonjs741@gmail.com>
2021-01-21 18:01:01 +0900

    Everybody Make SOME LOG!!

--- webdir/main.go
+++ webdir/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "Banosa_Project/webdir/server"
+import (
+	"Banosa_Project/webdir/server"
+	"fmt"
+)
 
 func main() {
+	// log TODO until 01/23
+	fmt.Println("START Banosa")
 	server.Run()
-
 }
