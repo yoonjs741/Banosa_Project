ea30e7000118494eaf06b84dfc2f7a56a32af569
윤지수 <yoonjs741@gmail.com>
2021-01-24 23:37:20 +0900

    Add Log file

--- webdir/main.go
+++ webdir/main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"Banosa_Project/webdir/server"
 	"fmt"
+	"log"
+	"os"
 )
 
+var Logger *log.Logger
+
 func main() {
-	// log TODO until 01/23
+	log.Println("Logging")
+	log.SetFlags(0)
+	fpLog, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer fpLog.Close()
+
+	Logger = log.New(fpLog, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	run()
+
+	Logger.Println("End of Program")
 	fmt.Println("START Banosa")
 	server.Run()
+}
+
+func run() {
+	Logger.Print("Test")
 }
