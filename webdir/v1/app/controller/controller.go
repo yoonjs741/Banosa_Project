27a77846d373dbf8627b27b4be8650dcddbe8fed
MA_THINKPAD <jsyun@monitorapp.com>
2021-02-01 12:56:21 +0900

    Add factorial input value verification

--- webdir/v1/app/controller/controller.go
+++ webdir/v1/app/controller/controller.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"Banosa_Project/webdir/v1/app/factorial"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
@@ -13,25 +15,38 @@ func HelloworldCTRL(c echo.Context) error {
 	return c.HTML(200, "<h2> Welcome to Banosa </h2>")
 }
 
+// HelloAssonCTRL : Return Asson string as http ok to CTRL
 func HelloAssonCTRL(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello Asson! Hm...")
 }
 
+// HelloDonjonCTRL : Return Donjon string as http ok to CTRL
 func HelloDonjonCTRL(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello DonJon! What should I do next :<")
 }
 
+// HelloQueryCTRL : Return Emdcoded QueryParams  as http ok to CTRL
 func HelloQueryCTRL(c echo.Context) error {
 	return c.String(http.StatusOK, c.QueryParams().Encode())
 }
 
+// HelloParamCTRL : Return QueryString as http ok to CTRL
 func HelloParamCTRL(c echo.Context) error {
 	return c.String(http.StatusOK, c.QueryString())
 }
 
+// HelloQueryFacto : Return QueryFacto string as http ok to CTRL
 func HelloQueryFacto(c echo.Context) error {
-	//id := c.QueryString()
-	id := c.QueryParam("id") //id의 값만 보게 변경
-	return c.JSON(200, factorial.GetFacto(id))
+	var StrconvErr error
 
+	id := c.QueryParam("id") //id의 값만 보게 변경
+
+	_, StrconvErr = strconv.Atoi(id)
+
+	fmt.Println(StrconvErr)
+	if StrconvErr == nil {
+		return c.JSON(200, factorial.GetFacto(id))
+	} else {
+		return c.String(http.StatusOK, "Please input only number.")
+	}
 }
